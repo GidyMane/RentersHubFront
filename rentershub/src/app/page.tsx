b276706b7c8e472b7d62bcfd8c4d36e725d67805
GidyMane <muragegideon2000@gmail.com>
2024-12-10 11:45:56 +0300

    update on work

---
+++ rentershub/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
+export default function Home() {
+  return (
+    
+      <main> <LoginLink>LOGIN</LoginLink>
+
+      
+
+<RegisterLink>Sign up</RegisterLink>
+              </main>
+         
+  );
+}
