d3109bd4aae34ff3988725a5cb4fc2b9da68b2e9
GidyMane <muragegideon2000@gmail.com>
2025-01-20 18:50:41 +0300

    updates on navbar

--- rentershub/src/components/Test/Rentershub/Navbar.tsx
+++ rentershub/src/components/Test/Rentershub/Navbar.tsx
@@ -49,14 +49,14 @@ export function Navbar({ toggleSidebar }: NavbarProps) {
                 <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                   <Avatar className="h-8 w-8">
                     <AvatarImage src="/placeholder.svg?height=32&width=32" alt="@username" />
-                    <AvatarFallback>MK</AvatarFallback>
+                    <AvatarFallback>MB</AvatarFallback>
                   </Avatar>
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent className="w-56" align="end" forceMount>
                 <DropdownMenuLabel className="font-normal">
                   <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">John Doe</p>
+                    <p className="text-sm font-medium leading-none">Muchiri Bundi</p>
                     <p className="text-xs leading-none text-muted-foreground">
                       muchiri.ke@example.com
                     </p>
