f273238145aae733c5b61a057301bf8b42eae5c0
Mane Software <85733336+GidyMane@users.noreply.github.com>
2025-02-17 06:03:47 +0300

    after success message

--- rentershub/src/components/SuccessMessage.tsx
+++ rentershub/src/components/SuccessMessage.tsx
@@ -50,7 +50,7 @@ const SuccessMessage = () => {
           <Button 
              
             className="bg-blue-600 hover:bg-blue-700 text-white py-6 px-8 text-lg"
-            onClick={() => router.push('/')}
+            onClick={() => router.push('/Testclient')}
           >
             Explore Platform
           </Button>
