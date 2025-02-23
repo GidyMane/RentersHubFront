1cbaf219633983443f1cc12c5d1ccd410b231f8c
kibochamark <mark.varu@student.cuk.ac.ke>
2025-02-24 00:35:50 +0300

    navbar

--- rentershub/src/components/admin/NavBardropdown.tsx
+++ rentershub/src/components/admin/NavBardropdown.tsx
@@ -34,25 +34,27 @@ import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { useUser } from "@/actions/useUser";
 
 
 export function NavBarDropDown() {
     const router = useRouter()
-    const { data:session } = useSession();
+    const { data: session } = useSession();
+    const { user } = useUser()
     //console.log(session, "user")
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild className="cursor-pointer transition-all duration-300">
                 <Avatar>
-                    <AvatarImage src="" />
-                    <AvatarFallback>{session?.user?.name?.charAt(0).toUpperCase()}</AvatarFallback>
+                                  <AvatarImage src={user.avatar} />
+                                  <AvatarFallback>{user.name.charAt(0).toUpperCase()}</AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56">
                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
                 <DropdownMenuItem>
-                    <div className="flex gap-2 hover:cursor-pointer focus:cursor-pointer after:bg-gray-100" onClick={()=>{
-                       router.push("/api/auth/signout") 
+                    <div className="flex gap-2 hover:cursor-pointer focus:cursor-pointer after:bg-gray-100" onClick={() => {
+                        router.push("/api/auth/signout")
                     }}>
                         <LogOut />
                         <span>Log out</span>
