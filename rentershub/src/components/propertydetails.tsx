836727fac472f8b6a3d223efb3a880b8b6312182
GidyMane <muragegideon2000@gmail.com>
2025-02-27 12:42:38 -0800

    update price look

--- rentershub/src/components/propertydetails.tsx
+++ rentershub/src/components/propertydetails.tsx
@@ -157,11 +157,13 @@ export default function PropertyDetail({
   }
 
   const formatCurrency = (amount: string | number) => {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "KES",
-    }).format(Number(amount))
-  }
+  return `Ksh ${new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "KES",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(Number(amount)).replace("KES", "").trim()}`;
+};
 
   return (
     <div className="mx-auto">
