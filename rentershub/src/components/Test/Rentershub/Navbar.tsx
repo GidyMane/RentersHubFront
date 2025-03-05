f17581b1b340f15dabc7fec547cb18a0bfac3803
GidyMane <muragegideon2000@gmail.com>
2025-03-05 09:36:34 +0300

    update the look

--- rentershub/src/components/Test/Rentershub/Navbar.tsx
+++ rentershub/src/components/Test/Rentershub/Navbar.tsx
@@ -97,15 +97,15 @@ export function Navbar({ toggleSidebar, isOpen }: NavbarProps) {
             </Button>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+                <Button variant="ghost" className="relative h-8 w-8 rounded-full" style={{ fontFamily: "Georgia, serif" }}>
                   <Avatar className="h-8 w-8">
                     <AvatarImage src={user.avatar} alt={user.name} />
                     <AvatarFallback>{user.name?.charAt(0)}</AvatarFallback>
                   </Avatar>
                 </Button>
               </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56" align="end" forceMount>
-                <DropdownMenuLabel className="font-normal">
+              <DropdownMenuContent className="w-56" align="end" style={{ fontFamily: "Georgia, serif" }} forceMount>
+                <DropdownMenuLabel className="font-normal" style={{ fontFamily: "Georgia, serif" }}>
                   <div className="flex flex-col space-y-1">
                     <p className="text-sm font-medium leading-none">{user.name}</p>
                     <p className="text-xs leading-none text-muted-foreground">
@@ -129,7 +129,7 @@ export function Navbar({ toggleSidebar, isOpen }: NavbarProps) {
               await signOut()
             }}
           >
-            <LogOut className="h-5 w-5 mr-3" aria-hidden="true" />
+            <LogOut className="h-5 w-5 mr-3" aria-hidden="true"  style={{ fontFamily: "Georgia, serif" }}/>
             Log Out
           </Button>
                 </DropdownMenuItem>
