1b7818d037a1ed391144f05891aada611ada5ebf
Mane Software <85733336+GidyMane@users.noreply.github.com>
2025-07-17 00:01:32 +0300

    Update page.tsx

--- rentershub/src/app/page.tsx
+++ rentershub/src/app/page.tsx
@@ -1,145 +1,145 @@
-// import { Suspense } from "react"
-// import { FullScreenCarousel } from "@/components/UpdatedLayout/HeroSections"
-// import { Loader } from "lucide-react"
-// import PropertyRender from "@/components/PropertyRender"
-// import { Pagination } from "@/components/pagination"
-// import { getpropertytype } from "../../data-access/actions/get-property-type"
-// import { getproperties } from "../../data-access/actions/getProperties"
-// import { updatePage } from "../../data-access/actions/updatePage"
+import { Suspense } from "react"
+import { FullScreenCarousel } from "@/components/UpdatedLayout/HeroSections"
+import { Loader } from "lucide-react"
+import PropertyRender from "@/components/PropertyRender"
+import { Pagination } from "@/components/pagination"
+import { getpropertytype } from "../../data-access/actions/get-property-type"
+import { getproperties } from "../../data-access/actions/getProperties"
+import { updatePage } from "../../data-access/actions/updatePage"
 
-// const key = process.env.GOOGLE_MAPS_API_KEY!
-// export const dynamic = "force-dynamic"
+const key = process.env.GOOGLE_MAPS_API_KEY!
+export const dynamic = "force-dynamic"
 
-// const page = async (props: {
-//   searchParams: Promise<{
-//     limit: number
-//     offset: number
-//     address: string
-//     propertytype_name: string
-//     rent_price_max: number
-//     special_condition: string
-//   }>
-// }) => {
-//   const params = await props.searchParams
-//   const limit = params?.limit || 20
-//   const offset = params?.offset || null
-//   const address = params?.address || null
-//   const propertytype_name = params?.propertytype_name || null
-//   const rent_price_max = params?.rent_price_max || null
-//   const special_condition = params?.special_condition || null
+const page = async (props: {
+  searchParams: Promise<{
+    limit: number
+    offset: number
+    address: string
+    propertytype_name: string
+    rent_price_max: number
+    special_condition: string
+  }>
+}) => {
+  const params = await props.searchParams
+  const limit = params?.limit || 20
+  const offset = params?.offset || null
+  const address = params?.address || null
+  const propertytype_name = params?.propertytype_name || null
+  const rent_price_max = params?.rent_price_max || null
+  const special_condition = params?.special_condition || null
 
-//   const formattedRentPrice = rent_price_max ? `KSh ${rent_price_max} pm` : "Not specified"
+  const formattedRentPrice = rent_price_max ? `KSh ${rent_price_max} pm` : "Not specified"
 
-//   const messageParts = [
-//     `Hello. I am from the website, https://rentershub.co.ke and I am searching for a vacant house that meets this criterion:`,
-//   ]
+  const messageParts = [
+    `Hello. I am from the website, https://rentershub.co.ke and I am searching for a vacant house that meets this criterion:`,
+  ]
 
-//   if (propertytype_name) messageParts.push(`HOUSE TYPE: ${propertytype_name}`)
-//   if (address) messageParts.push(`LOCATION: ${address}`)
-//   if (rent_price_max) messageParts.push(`MAX RENT BUDGET: ${formattedRentPrice}`)
-//   if (special_condition) messageParts.push(`CONDITION: ${special_condition}`)
+  if (propertytype_name) messageParts.push(`HOUSE TYPE: ${propertytype_name}`)
+  if (address) messageParts.push(`LOCATION: ${address}`)
+  if (rent_price_max) messageParts.push(`MAX RENT BUDGET: ${formattedRentPrice}`)
+  if (special_condition) messageParts.push(`CONDITION: ${special_condition}`)
 
-//   messageParts.push(`Please help me find the house. Thank you.`)
+  messageParts.push(`Please help me find the house. Thank you.`)
 
-//   const whatsappMessage = encodeURIComponent(messageParts.join("\n\n\n"))
+  const whatsappMessage = encodeURIComponent(messageParts.join("\n\n\n"))
 
-//   const whatsappLink = `https://wa.me/254731352350?text=${whatsappMessage}`
+  const whatsappLink = `https://wa.me/254731352350?text=${whatsappMessage}`
 
-//   const [apiproperties, propertytypes] = await Promise.all([
-//     getproperties(limit, offset, address, propertytype_name, rent_price_max, special_condition),
-//     getpropertytype(),
-//   ])
+  const [apiproperties, propertytypes] = await Promise.all([
+    getproperties(limit, offset, address, propertytype_name, rent_price_max, special_condition),
+    getpropertytype(),
+  ])
 
-//   return (
-//     <Suspense>
-//       <div className="h-full w-full bg-background font-georgia">
-//         <div className="h-screen relative w-full">
-//           <Suspense fallback={<Loader className="animate animate-spin text-secondary400" />}>
-//             <FullScreenCarousel propertytype={propertytypes} api_key={key} />
-//           </Suspense>
-//         </div>
-//         <div className="flex items-center justify-center md:px-10">
-//           <div className="my-2"></div>
-//         </div>
-//         <div
-//           id="search-results"
-//           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:px-16 px-6"
-//         >
-//           <Suspense
-//             fallback={
-//               <div className="col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4 flex items-center justify-center my-4">
-//                 <Loader className="text-primary w-6 h-6 animate-spin" />
-//               </div>
-//             }
-//           >
-//             {apiproperties[0] == 200 && apiproperties[1].results.length > 0 ? (
-//               apiproperties[1].results.map((property: any, idx: number) => (
-//                 <PropertyRender property={property} key={idx} />
-//               ))
-//             ) : (
-//               <div
-//                 className="col-span-full flex flex-col items-start justify-center bg-white p-8 rounded-xl w-full md:w-3/5 mx-auto text-left"
-//                 style={{ fontFamily: "Georgia, serif" }}
-//               >
-//                 <p className="mt-4 text-gray-700 text-lg leading-relaxed">
-//                   Hello. Landlords and Property Agents are yet to post such a house in that location.
-//                   <br />
-//                   <br />
-//                   Please go back and search another house or a different location or.
-//                   <br />
-//                   <br />
-//                   Contact our admin for further assistance.
-//                 </p>
-//                 <div className="flex flex-col md:flex-row gap-6 mt-6 w-full">
-//                   <a
-//                     href="tel:+254731352350"
-//                     className="px-6 py-3 text-lg font-semibold rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition transform hover:scale-105"
-//                   >
-//                     📞 Call Admin
-//                   </a>
-//                   <a
-//                     href={whatsappLink}
-//                     target="_blank"
-//                     rel="noopener noreferrer"
-//                     className="px-6 py-3 text-lg font-semibold rounded-lg bg-green-500 text-white hover:bg-green-600 transition transform hover:scale-105"
-//                   >
-//                     💬 Chat With Admin
-//                   </a>
-//                 </div>
-//               </div>
-//             )}
-//           </Suspense>
-//         </div>
-//         <div>
-//           {apiproperties[0] == 200 && apiproperties[1]?.results.length > 0 && (
-//             <Pagination
-//               count={apiproperties[0] == 200 ? Math.round(apiproperties[1].count / 20) : 0}
-//               previous={""}
-//               next={""}
-//               updatePage={updatePage}
-//             />
-//           )}
-//         </div>
-//       </div>
-//     </Suspense>
-//   )
-// }
-
-// export default page
-
-import { MaintenancePage } from "@/components/MaintenancePage"
-
-export default function Home() {
   return (
-    <MaintenancePage
-      estimatedUptime="Friday at 12:00 PM EST"
-      contactEmail="support@rentershub.com"
-      socialLinks={{
-        twitter: "https://www.linkedin.com/company/renters-hub",
-        facebook: "https://www.facebook.com/rentershub.co.ke?mibextid=wwXIfr&mibextid=wwXIfr",
-        instagram: "https://www.instagram.com/rentershub.co.ke?igsh=MTVzcmh5Z2EwY3IwZg==",
-      }}
-    />
+    <Suspense>
+      <div className="h-full w-full bg-background font-georgia">
+        <div className="h-screen relative w-full">
+          <Suspense fallback={<Loader className="animate animate-spin text-secondary400" />}>
+            <FullScreenCarousel propertytype={propertytypes} api_key={key} />
+          </Suspense>
+        </div>
+        <div className="flex items-center justify-center md:px-10">
+          <div className="my-2"></div>
+        </div>
+        <div
+          id="search-results"
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:px-16 px-6"
+        >
+          <Suspense
+            fallback={
+              <div className="col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4 flex items-center justify-center my-4">
+                <Loader className="text-primary w-6 h-6 animate-spin" />
+              </div>
+            }
+          >
+            {apiproperties[0] == 200 && apiproperties[1].results.length > 0 ? (
+              apiproperties[1].results.map((property: any, idx: number) => (
+                <PropertyRender property={property} key={idx} />
+              ))
+            ) : (
+              <div
+                className="col-span-full flex flex-col items-start justify-center bg-white p-8 rounded-xl w-full md:w-3/5 mx-auto text-left"
+                style={{ fontFamily: "Georgia, serif" }}
+              >
+                <p className="mt-4 text-gray-700 text-lg leading-relaxed">
+                  Hello. Landlords and Property Agents are yet to post such a house in that location.
+                  <br />
+                  <br />
+                  Please go back and search another house or a different location or.
+                  <br />
+                  <br />
+                  Contact our admin for further assistance.
+                </p>
+                <div className="flex flex-col md:flex-row gap-6 mt-6 w-full">
+                  <a
+                    href="tel:+254731352350"
+                    className="px-6 py-3 text-lg font-semibold rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition transform hover:scale-105"
+                  >
+                    📞 Call Admin
+                  </a>
+                  <a
+                    href={whatsappLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-6 py-3 text-lg font-semibold rounded-lg bg-green-500 text-white hover:bg-green-600 transition transform hover:scale-105"
+                  >
+                    💬 Chat With Admin
+                  </a>
+                </div>
+              </div>
+            )}
+          </Suspense>
+        </div>
+        <div>
+          {apiproperties[0] == 200 && apiproperties[1]?.results.length > 0 && (
+            <Pagination
+              count={apiproperties[0] == 200 ? Math.round(apiproperties[1].count / 20) : 0}
+              previous={""}
+              next={""}
+              updatePage={updatePage}
+            />
+          )}
+        </div>
+      </div>
+    </Suspense>
   )
 }
+
+export default page
+
+// import { MaintenancePage } from "@/components/MaintenancePage"
+
+// export default function Home() {
+//   return (
+//     <MaintenancePage
+//       estimatedUptime="Friday at 12:00 PM EST"
+//       contactEmail="support@rentershub.com"
+//       socialLinks={{
+//         twitter: "https://www.linkedin.com/company/renters-hub",
+//         facebook: "https://www.facebook.com/rentershub.co.ke?mibextid=wwXIfr&mibextid=wwXIfr",
+//         instagram: "https://www.instagram.com/rentershub.co.ke?igsh=MTVzcmh5Z2EwY3IwZg==",
+//       }}
+//     />
+//   )
+// }
 
